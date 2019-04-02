2219901bf637b2a8d180bab665bbbc9c48c959ac
marte <iqianduan@126.com>
2019-04-02 16:12:26 +0800

    删除了columns所以列的title，动态根据字典补充title

--- src/views/buy/utils/columns.js
+++ src/views/buy/utils/columns.js
@@ -1,9 +1,10 @@
 import moment from "moment"
+import dictionary from "./dictionary"
 
 export default (store) =>{
     let o = [
         {
-            title: '缩略图', key: 'avatar',
+            key: 'avatar',
             render(h, { row }) {
                 // 这是JSX语法
                 return h('div', { attrs: { 'data-id': row.id } }, [
@@ -17,24 +18,24 @@ export default (store) =>{
                 ])
             }
         },
-        { title: '编号', key: 'id', sortable: 'custom', width: 85 },
-        { title: '品牌', key: 'brand', width:70 },
-        { title: '型号', key: 'series', width: 80 },
-        { title: '颜色', key: 'color', width: 55 },
-        { title: '价格', key: 'price', sortable: 'custom' },
-        { title: '档位', key: 'gearbox', width: 60 },
-        { title: '排量', key: 'engine', width: 60},
-        { title: '环保', key: 'exhaust', width: 60},
-        { title: '公里数', key: 'km', sortable: 'custom' },
+        { key: 'id', sortable: 'custom', width: 85 },
+        { key: 'brand', width:70 },
+        { key: 'series', width: 80 },
+        { key: 'color', width: 55 },
+        { key: 'price', sortable: 'custom' },
+        { key: 'gearbox', width: 60 },
+        { key: 'engine', width: 60},
+        { key: 'exhaust', width: 60},
+        { key: 'km', sortable: 'custom' },
         { 
             title: '购买时间', key: 'buydate',
             render(h, {row}){
                 return h('div', {}, moment(row.buydate).format('YYYY年MM月DD日'))
             }
         },
-        { title: '燃料', key: 'fuel', width: 85 },
+        {  key: 'fuel', width: 85 },
         {
-            title: '是否含牌', key: 'license',
+            key: 'license',
             render(h, { row }) {
                 return h('div', {}, row.license ? '是' : '否')
             }
@@ -44,6 +45,9 @@ export default (store) =>{
     
     // 遍历o数组，看看o数组当中谁的key与store中的sort中的sortby相同，就表示按这个对象排序
     for(let i = 0;i < o.length;i++){
+        // 补充title属性
+        // 将o对象中的title全删除，动态从dictionary中读取key，给每一项补充title属性
+        o[i].title = dictionary[o[i].key]
         if(o[i].key === store.state.largeTableStore.sort.sortby){
             // 补充sortType属性，让他有默认初始排序
             o[i].sortType = store.state.largeTableStore.sort.sortdirection == 1 ? 'asc' : 'desc'
@@ -64,5 +68,5 @@ export default (store) =>{
         })
     }
 
-    // return o;
+
 }
