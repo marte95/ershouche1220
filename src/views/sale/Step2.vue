e47947cc967f09620d66ce96cb367c32f6713416
marte <iqianduan@126.com>
2019-04-03 10:16:37 +0800

    使用HTML5的FileReader构造函数实现图片上传前预览，onprogress事件实现上传进度

--- src/views/sale/Step2.vue
+++ src/views/sale/Step2.vue
@@ -2,19 +2,22 @@
     <div>
         <h1>我是步骤2</h1>
 
-        <!-- <form action="http://192.168.1.88/upload" method="post" enctype="multipart/form-data">
-            <input type="file" name="file">
-            <input type="submit">
-        </form> -->
+        <input type="file" ref="fileCtrl" @change="changeHandle">
+        <!-- <Button @click="upload">上传</Button> -->
 
-        <input type="file" ref="fileCtrl">
-        <Button @click="upload">上传</Button>
-
+        <p><img width="80" :src="preURL" v-show="preURL"></p>
+        <p><Progress :percent="percent" status="active" /></p>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return {
+                preURL: '',
+                percent: 0
+            }
+        },
         methods: {
             upload(){
                 // 创建虚拟表单
@@ -25,19 +28,47 @@
 
                 // 发起Ajax请求上传图片
                 const xhr = new XMLHttpRequest()
+
+                // 上传进度事件，进行计算进步比例
+                xhr.upload.onprogress = (e) => {
+                    console.log(e)
+                    this.percent = e.loaded / e.total * 100
+                }
+
+                // 成功的回调函数
                 xhr.onreadystatechange = function () {
                     if(xhr.readyState === 4){
                         console.log(xhr.response)
                     }
                 }
 
-                xhr.open('post', 'http://192.168.1.88/upload', true);
+                // xhr.open('post', 'http://192.168.1.88/upload', true);
+                // 由于onprogress上传进度事件不支持CORE跨域，所以使用代理跨域上传
+                xhr.open('post', '/api/upload', true);
                 xhr.send(form);  //要发送的内容
+            },
+            
+            // 用户选择好图之后的事情
+            changeHandle(){
+                // 创建文件读取器
+                const fileReader = new FileReader();
+                // 读取用户选择的文件内容，读为URL地址（就是base64编号图片）
+                fileReader.readAsDataURL(this.$refs.fileCtrl.files[0]) 
+                
+                // 读取完毕（此时还没上传）
+                fileReader.onload = (e)=>{
+                    this.preURL = e.target.result  //上传好图片，预览
+
+                    this.upload();  //选好图自动执行上传图片事件
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+    p{
+        width: 300px;
+        margin:10px 0;
+    }
 </style>
